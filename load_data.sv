8a2597df1a322ed2cf90eecfea2ad707012e7cd0
Varun Venkatesh <varunven@uw.edu>
2023-03-08 16:28:01 -0800

    load_data verilator

--- load_data.sv
+++ load_data.sv
@@ -20,41 +20,42 @@ module load_data(
 			3'b010: reg_write_data = MEMWB_data_read;                                   		//LW
 			3'b100: reg_write_data = {24'b0, MEMWB_data_read[offset +: 8]};             		//LBU
 			3'b101: reg_write_data = {16'b0, MEMWB_data_read[offset +: 16]};            		//LHU
+			default: begin end
 			endcase
 	  end
 	end
 endmodule
 
-module load_data_testbench ();
-	logic clk;
-	logic [6:0] MEMWB_opcode_out;
-	logic [2:0] MEMWB_funct3_out;
-	logic [31:0] MEMWB_data_addr_out, MEMWB_data_read, MEMWB_reg_write_data_out;
-	logic [31:0] reg_write_data;
+// module load_data_testbench ();
+// 	logic clk;
+// 	logic [6:0] MEMWB_opcode_out;
+// 	logic [2:0] MEMWB_funct3_out;
+// 	logic [31:0] MEMWB_data_addr_out, MEMWB_data_read, MEMWB_reg_write_data_out;
+// 	logic [31:0] reg_write_data;
  
- 	// Instantiating modules
-	load_data dut(.clk, .MEMWB_opcode_out, .MEMWB_funct3_out, .MEMWB_data_addr_out,
-	.MEMWB_data_read, .MEMWB_reg_write_data_out, .reg_write_data);
+//  	// Instantiating modules
+// 	load_data dut(.clk, .MEMWB_opcode_out, .MEMWB_funct3_out, .MEMWB_data_addr_out,
+// 	.MEMWB_data_read, .MEMWB_reg_write_data_out, .reg_write_data);
 	
-	parameter CLOCK_PERIOD=10;
- 	initial begin
- 		clk <= 0;
- 		forever #(CLOCK_PERIOD/2) clk <= ~clk;
- 	end
+// 	parameter CLOCK_PERIOD=10;
+//  	initial begin
+//  		clk <= 0;
+//  		forever #(CLOCK_PERIOD/2) clk <= ~clk;
+//  	end
 	
- 	initial begin
-		MEMWB_opcode_out = 1;
-		MEMWB_funct3_out = 11;
-		MEMWB_data_addr_out = 32'b00000000111000000000000000000001;
-		MEMWB_data_read = 32'b00000000000000000000000000000011;
-		MEMWB_reg_write_data_out = 12; #50
+//  	initial begin
+// 		MEMWB_opcode_out = 1;
+// 		MEMWB_funct3_out = 11;
+// 		MEMWB_data_addr_out = 32'b00000000111000000000000000000001;
+// 		MEMWB_data_read = 32'b00000000000000000000000000000011;
+// 		MEMWB_reg_write_data_out = 12; #50
 		
-		MEMWB_opcode_out = 7'b0000011;
-		MEMWB_funct3_out = 3'b000; #50
-		MEMWB_funct3_out = 3'b001; #50
-		MEMWB_funct3_out = 3'b010; #50
-		MEMWB_funct3_out = 3'b100; #50
-		MEMWB_funct3_out = 3'b101; #50
- 		$stop;
- 	end
-endmodule
+// 		MEMWB_opcode_out = 7'b0000011;
+// 		MEMWB_funct3_out = 3'b000; #50
+// 		MEMWB_funct3_out = 3'b001; #50
+// 		MEMWB_funct3_out = 3'b010; #50
+// 		MEMWB_funct3_out = 3'b100; #50
+// 		MEMWB_funct3_out = 3'b101; #50
+//  		$stop;
+//  	end
+// endmodule
